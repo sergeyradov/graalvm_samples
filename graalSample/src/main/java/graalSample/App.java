0632303f4f8a94d1b4e9de4d4c9c32c53a9b3a8d
Sergey Radov <serghei.radov@oracle.com>
2018-09-12 15:07:34 +0300

    Updated gitignore

---
+++ graalSample/src/main/java/graalSample/App.java
@@ -0,0 +1,34 @@
+package graalSample;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String printNumberScript = "console.log(phone.number)";
+        String callingScript = "phone.call('Someone')";
+
+        Phone phone = new Phone(12345);
+
+        Context jsContext = Context.create("js");
+
+        jsContext.getBindings("js").putMember("phone",phone);
+
+        runScript(printNumberScript,jsContext);
+        runScript(callingScript,jsContext);
+
+        runScript("console.log('Hello from the project')", jsContext);
+
+    }
+
+    private static Value runScript(String script, Context context) {
+        return context.eval("js", script);
+    }
+}
+
